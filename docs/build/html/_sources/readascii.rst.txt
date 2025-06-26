2230c0674ab942cd60700fce03b68aec419ac619
matheus-bp <math.bp.sci@proton.me>
2025-06-26 10:18:38 +0200

    adding(testig) documentation with Sphinx

---
+++ docs/build/html/_sources/readascii.rst.txt
@@ -0,0 +1,7 @@
+readascii module
+================
+
+.. automodule:: readascii
+   :members:
+   :show-inheritance:
+   :undoc-members:
---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'TooPAN'
+copyright = '2025, M. Bernini-Peron'
+author = 'M. Bernini-Peron'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src/'))
+extensions = ['sphinx.ext.autodoc',
+              'myst_parser',
+              'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'  # Use the Read the Docs theme
+html_static_path = ['_static']
